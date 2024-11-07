ce4dfcb8400d578f88743f8c18056797e1480601
JitenderMX <jitender.work.mediax@gmail.com>
2024-11-07 12:45:12 +0530

    new project

--- app.js
+++ app.js
@@ -93,6 +93,6 @@ app.post('/verify', async (req, res) => {
     }
 })
 
-app.listen(8000, () => {
-    console.log('Server is running on port 8000');
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 })
